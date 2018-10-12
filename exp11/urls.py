58177ef493f1a2ac25d8503925684bb4d3be5077
mohammak <GitHubSk@112>
2018-10-12 16:26:43 +0530

    'added restframework in settings.py'

--- exp11/urls.py
+++ exp11/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(r'^api-auth/', include('rest_framework.urls')),
     path(r'q/', include('myapp.urls')),
 ]
